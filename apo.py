2e43b94da6e81c83e0e1507f7c79d066c7c289fa
damphon <waddledoocanto@gmail.com>
2023-05-27 09:07:01 -0700

    DOCS: Researched and filled out TODO list

--- apo.py
+++ apo.py
@@ -1,11 +1,16 @@
 # TODO Make a nice heading for this code
 # TODO Get a list of files in a directory
+### https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
 # TODO Loop through each file
 # TODO How do I read metadata/exif data?
-# TODO rename the file
+### https://pillow.readthedocs.io/en/stable/reference/ExifTags.html
 # TODO Does needed directory exist?
+### if os.path.exists('file_path')
 # TODO Make the needed directory
-# TODO move the file
+### os.mkdir(path to directory)
+### https://www.geeksforgeeks.org/create-a-directory-in-python/#
+# TODO rename and move the file
+### https://stackoverflow.com/questions/2491222/how-to-rename-a-file-using-python
 # TODO How do I make a requirements file?
 # TODO Make sure readme file has all instructions for working with the code
 
