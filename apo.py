a3da4b09d05cc39a3b6cac300f620850c69eb0b0
Jeremy Toler <waddledoocanto@gmail.com>
2023-05-29 10:59:29 -0700

    FEAT: Get list of files
    Added basic structure, some more todo list items, and I get a list of files and loop through them.

--- apo.py
+++ apo.py
@@ -1,7 +1,4 @@
 # TODO Make a nice heading for this code
-# TODO Get a list of files in a directory
-### https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
-# TODO Loop through each file
 # TODO How do I read metadata/exif data?
 ### https://pillow.readthedocs.io/en/stable/reference/ExifTags.html
 # TODO Does needed directory exist?
@@ -11,6 +8,44 @@
 ### https://www.geeksforgeeks.org/create-a-directory-in-python/#
 # TODO rename and move the file
 ### https://stackoverflow.com/questions/2491222/how-to-rename-a-file-using-python
+# TODO Test jpg
+# TODO Test png
+# TODO Test gif
+# TODO Test bmp
+# TODO Test very old photos
+# TODO Add video support
+# TODO Test mp4
+# TODO Test avi
+# TODO Test mkv
+# TODO Test very old video
 # TODO How do I make a requirements file?
 # TODO Make sure readme file has all instructions for working with the code
 
+from os import walk
+import PIL
+
+unsorted_path = 'C:\\Users\\Ueno\\Pictures\\test\\unsorted'
+sorted_path = 'C:\\Users\\Ueno\\Pictures\\test\\photos'
+
+def get_files(unsorted_path):
+    files = []
+    for (dirpath, dirnames, filenames) in walk(unsorted_path):
+        files.extend(filenames)
+        break # Stops walk from adding filenames in subdirectories.
+    return files
+
+def get_metadata(file):
+    pass
+
+def sort_file(file):
+    pass
+
+def needs_manual_sort(file):
+    pass
+
+def main():
+    for file in get_files(unsorted_path):
+        sort_file(f'{unsorted_path}\\{file}')
+
+if __name__ == '__main__':
+    main()
