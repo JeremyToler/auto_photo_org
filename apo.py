5571adb34f94a1801150ee77bf4e981796ff2014
Jeremy Toler <waddledoocanto@gmail.com>
2023-06-13 10:50:48 -0700

    FIX: Added error handling when getting the metadata
    I don't want the code to error and stop if a file cannot be processed. Added try statement to get_metadata so if pillow cannot open the file as an image, or if the image type does not have exif data it just returns an empty meta_dict and the error handling already present in main will skip the file and log it.
    
    I was also able to bring get_metadata to only 3 indentations! This makes it Linux code style compliant ;)
    
    "if you need more than 3 levels of indentation, you're screwed anyway, and should fix your program."
    
    https://www.kernel.org/doc/html/latest/process/coding-style.html

--- apo.py
+++ apo.py
@@ -1,7 +1,3 @@
-# TODO Test png
-# TODO Test gif
-# TODO Test bmp
-# TODO Test very old photos
 # TODO Add video support
 # TODO Test mp4
 # TODO Test avi
@@ -11,8 +7,6 @@
 # pip install pillow, geopy
 # TODO How do I make a requirements file?
 # TODO Make sure readme file has all instructions for working with the code
-# TODO Better logs (aut remove old logs, file per run)
-# TimedRotatingFileHandler? part of logging module.
 
 import os
 from PIL import Image
@@ -27,28 +21,40 @@ def get_files(unsorted_path):
     files = []
     for (dirpath, dirnames, filenames) in os.walk(unsorted_path):
         files.extend(filenames)
-        break # Stops walk from adding filenames in subdirectories.
+        # Stop walk from adding filenames in subdirectories.
+        # TODO Change of plans, lets include subdirectories. 
+        break 
     return files
 
 def get_metadata(file_path):
     meta_dict = {}
-    image = Image.open(file_path)
-    exif = image._getexif()
-    if exif:
-        for tag, value in exif.items():
-            decoded = TAGS.get(tag, tag)
-            if decoded == 'DateTime':
-                meta_dict[decoded] = value
-            if decoded == 'GPSInfo':
-                for gps_tag in value:
-                    gps_decoded = GPSTAGS.get(gps_tag, gps_tag)
-                    meta_dict[gps_decoded] = value[gps_tag]
+    try:
+        image = Image.open(file_path)
+        exif = image._getexif()
+    except:
+        return meta_dict
+    if not exif:
+        return meta_dict
+    
+    for tag, value in exif.items():
+        decoded = TAGS.get(tag, tag)
+        if decoded == 'DateTime':
+            meta_dict[decoded] = value
+        if decoded == 'GPSInfo':
+            for gps_tag in value:
+                gps_decoded = GPSTAGS.get(gps_tag, gps_tag)
+                meta_dict[gps_decoded] = value[gps_tag]
     return meta_dict  
 
+# Converts time from '2023:01:15 18:25:07' to '2023-01-15.182507.'
 def process_timestamp(timestamp):
     date_object = datetime.strptime(timestamp, f'%Y:%m:%d %H:%M:%S')
     return date_object.strftime(f'%Y-%m-%d.%H%M%S') + '.'
 
+'''
+Use geopy to interact with the Nominatim (OpenStreetMap) API
+At zoom level 10 Address returns City, County, State, Country
+'''
 def process_gps(meta_dict):
     geolocator = Nominatim(user_agent='auto_photo_org')
     latitude = convert_gps(*meta_dict['GPSLatitude'], 
@@ -84,31 +90,32 @@ def sort_file(old, new):
     )
 
 def main():
+    # TODO Better logging, with logging module
     logtime = datetime.now()
     log = open('log.txt', 'a')
     print(logtime, 'Starting Batch Rename', file=log)
     for filename in get_files(unsorted_path):
         print(logtime, 'Processing:', filename, file=log)
         meta_dict = get_metadata(os.path.join(unsorted_path, filename))
-        if not meta_dict:
-            print('No Metadata:', filename, file=log)
-            continue # Skip files that dont have Metadata
+        if not meta_dict: # Skip files that dont have Metadata
+            print(logtime, 'No Metadata:', filename, file=log)
+            continue 
         if 'DateTime' in meta_dict:
             time = process_timestamp(meta_dict['DateTime'])
-        else:
+        else: # Skip files that dont have date/time
             print(logtime, 'No DateTime:', filename, file=log)
-            continue # Skip files that dont have date/time
+            continue 
         if 'GPSLatitude' in meta_dict:
             city = process_gps(meta_dict)
         else:
             print(logtime, 'No GPS:', filename, file=log)
             city = ''
-        # keep the ext and differentiate photos taken within 1 second
+        # TODO Make a better per file stamp. More reliably get the ext. 
         end = filename[-7:]
         new_filename = f'{time}{city}{end}'
         sort_file(filename, new_filename)
         print(logtime, 'Success: New filename =', new_filename, file=log)
-        log.close()
+    log.close()
 
 if __name__ == '__main__':
     main()
