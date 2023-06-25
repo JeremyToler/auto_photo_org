855c4fff5b2dc08225ee7ce9d4d7b6e0fcd555cd
Jeremy Toler <waddledoocanto@gmail.com>
2023-06-25 14:30:43 -0700

    FEAT: Config File and Logging
    Started using the logging module and added a config file so that there is one place users go to update the paths.

--- apo.py
+++ apo.py
@@ -1,3 +1,6 @@
+'''
+    PAVONINE - Photo And Video Organizer
+'''
 # TODO Add video support
 # TODO Test mp4
 # TODO Test avi
@@ -9,13 +12,26 @@
 # TODO Make sure readme file has all instructions for working with the code
 
 import os
+import config
+import logging
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 from datetime import datetime
 from geopy.geocoders import Nominatim
 
-unsorted_path = 'C:\\Users\\Ueno\\Pictures\\test\\unsorted'
-sorted_path = 'C:\\Users\\Ueno\\Pictures\\test\\photos'
+if config.debug_mode:
+    loglevel = logging.DEBUG
+else:
+    loglevel = logging.INFO
+
+logging.basicConfig(
+    filename='apo.log',
+    filemode='w',
+    format='%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s',
+    level=loglevel
+)
+
+logging = logging.getLogger('APO')
 
 def get_files(unsorted_path):
     files = []
@@ -53,14 +69,16 @@ def get_metadata(file_path):
 # Converts time from '2023:01:15 18:25:07' to '2023-01-15.182507.'
 def process_timestamp(timestamp):
     date_object = datetime.strptime(timestamp, f'%Y:%m:%d %H:%M:%S')
-    return date_object.strftime(f'%Y-%m-%d.%H%M%S') + '.'
+    date = date_object.strftime(f'%Y-%m-%d')
+    time = date_object.strftime(f'%H%M%S')
+    return {'date': date, 'time': time}
 
 '''
 Use geopy to interact with the Nominatim (OpenStreetMap) API
 At zoom level 10 Address returns City, County, State, Country
 '''
 def process_gps(meta_dict):
-    geolocator = Nominatim(user_agent='auto_photo_org')
+    geolocator = Nominatim(user_agent=config.nominatum_agent)
     latitude = convert_gps(*meta_dict['GPSLatitude'], 
                            meta_dict['GPSLatitudeRef'])
     longitude = convert_gps(*meta_dict['GPSLongitude'],
@@ -70,7 +88,8 @@ def process_gps(meta_dict):
         zoom = 10,
         language='en-us'
         )
-    return location.address.split(', ', 1)[0] + '.'
+    city = location.address.split(', ', 1)[0]
+    return '.' + city.replace(' ', '_')
 
 '''
 Pillow returns GPS coordanats as Degrees, Minutes, Seconds
@@ -83,50 +102,42 @@ def convert_gps(deg, min, sec, ref):
     return result
 
 def sort_file(old, new):
-    new_path = os.path.join(sorted_path, new[:4])
-    if not os.path.exists(sorted_path):
-        os.mkdir(sorted_path)
+    new_path = os.path.join(config.sorted_path, new[:4])
+    new_file = os.path.join(new_path, new)
+    old_file = os.path.join(config.unsorted_path, old)
+    if not os.path.exists(config.sorted_path):
+        os.mkdir(config.sorted_path)
     if not os.path.exists(new_path):
         os.mkdir(new_path)
-    os.rename(
-        os.path.join(unsorted_path, old),
-        os.path.join(new_path, new)
-    )
+    os.rename(old_file, new_file)
+    logging.info(f'{old_file} has been renamed {new_file}')
 
 def main():
-    # TODO Better logging, with logging module
-    logtime = datetime.now()
-    log = open('log.txt', 'a')
-    print(logtime, 'Starting Batch Rename', file=log)
-
     # Numbering files so that no 2 files will have the same name
     i = 0 
-
-    for filename in get_files(unsorted_path):
-        print(logtime, 'Processing:', filename, file=log)
-        meta_dict = get_metadata(os.path.join(unsorted_path, filename))
+    for filename in get_files(config.unsorted_path):
+        logging.debug(f'Proccessing {filename}')
+        meta_dict = get_metadata(os.path.join(config.unsorted_path, filename))
 
         # TODO Move this error checking to its own function.
         if not meta_dict: 
-            print(logtime, 'No Metadata:', filename, file=log)
+            logging.warning(f'{filename} has no METADATA')
             continue 
         if 'DateTime' in meta_dict:
-            time = process_timestamp(meta_dict['DateTime'])
+            datetime = process_timestamp(meta_dict['DateTime'])
         else: 
-            print(logtime, 'No DateTime:', filename, file=log)
+            logging.warning(f'{filename} has no DATETIME Data')
             continue 
         if 'GPSLatitude' in meta_dict:
             city = process_gps(meta_dict)
         else:
-            print(logtime, 'No GPS:', filename, file=log)
+            logging.warning(f'{filename} has no GPS Data')
             city = ''
 
-        ext = filename.rsplit('.', 1)[0]
-        new_filename = f'{time}{city}{i}.{ext}'
+        ext = filename.rsplit('.', 1)[1]
+        new_filename = f'{datetime["date"]}.{datetime["time"]}{i}{city}.{ext}'
         sort_file(filename, new_filename)
-        print(logtime, 'Success: New filename =', new_filename, file=log)
         i += 1
-    log.close()
 
 if __name__ == '__main__':
     main()
---
+++ config.py
@@ -0,0 +1,14 @@
+# Path to image files that will be renamed and sorted
+unsorted_path = '/path/to/unsorted_photos'
+
+# Path you want the images saved
+sorted_path = '/path/to/photos'
+
+'''
+Nominatum (Open Street Maps) is used to proccess GPS data. 
+Put in any name so Nominatum can track useage.
+Leaving this blank may get you blocked.
+'''
+nominatum_agent = ''
+
+debug_mode = False
