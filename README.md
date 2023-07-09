153ef1f92a07a924d297576ed7653292881715c6
Jeremy Toler <waddledoocanto@gmail.com>
2023-07-09 11:47:10 -0700

    Revert "Removing ext from filename before extracting date"
    This reverts commit 65f8f0369a9fc5cd26edcbdeee4a4e5c72eb81ed.

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # Automatic Photo Organizer
 Automatically rename photos and move them to the correct directories. 
-
-
-Move apo.py and config.py to server
-chmod +x apo.py
-chmod +x config.py
-
-pip install geopy
-pip install pillow
-
--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Path to image files that will be renamed and sorted
-unsorted_path = 'C:\\Users\\Ueno\\Pictures\\test\\unsorted'
+unsorted_path = '/path/to/unsorted_photos'
 
 # Path you want the images saved
-sorted_path = 'C:\\Users\\Ueno\\Pictures\\test\\photos'
+sorted_path = '/path/to/photos'
 
 '''
 Nominatum (Open Street Maps) is used to proccess GPS data. 
 Put in any name so Nominatum can track useage.
 Leaving this blank may get you blocked.
 '''
-nominatum_agent = 'JeremyT_APO'
+nominatum_agent = ''
 
 debug_mode = False
