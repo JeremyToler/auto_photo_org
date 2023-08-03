66845be759f878601d4dd0ac4324a1dc51f92ab3
Jeremy Toler <waddledoocanto@gmail.com>
2023-08-03 13:38:47 -0700

    Renamed nominatum_agent to user_agent

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Path to image files that will be renamed and sorted
-unsorted_path = '/path/to/unsorted_photos'
+unsorted_path = 'C:\\Users\\Jeremy\\Pictures\\test\\unsorted'
 
 # Path you want the images saved
-sorted_path = '/path/to/photos'
+sorted_path = 'C:\\Users\\Jeremy\\Pictures\\test'
 
 '''
 Nominatum (Open Street Maps) is used to proccess GPS data. 
 Put in any name so Nominatum can track useage.
 Leaving this blank may get you blocked.
 '''
-nominatum_agent = ''
+user_agent = ''
 
 debug_mode = False
