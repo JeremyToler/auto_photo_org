0ba108d134a23663df63e1eb2182b954e922392e
Jeremy Toler <waddledoocanto@gmail.com>
2023-10-29 13:25:36 -0700

    Added setup script to be run when docker is made

--- apo_setup.py
+++ apo_setup.py
@@ -1,4 +1,16 @@
 import random
 import string
+import yaml
 
-uid = 'APO'.join(random.choices(string.ascii_letters + string.digits, k=10))
+def main():
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    uid = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    config['user_agent'] = f'APO-{uid}'
+
+    with open('config.yaml', 'w') as file:
+        yaml.dump(config, file)
+
+if __name__ == '__main__':
+    main()
